c10e0a72c304e6b8569071a7f35bb39bd2aafbfc
CalSouth <naml124@gmail.com>
2015-08-20 23:20:15 -0700

    fuck you git hub
    fuck yeah

---
+++ SimonSays.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import Multipeer Connectivity
+
+var str = "Hello, playground"
+var playerName1: String? = "Player 1"
+var playerName2: String? = "Player 2"
+var playerName3: String? = "Player 3"
+var playerName4: String? = "Player 4"
+var playerName5: String? = "Player 5"
+var playerName6: String? = "Player 6"
+var playerName7: String? = "Player 7"
+var playerName8: String? = "Player 8"
+
+if
